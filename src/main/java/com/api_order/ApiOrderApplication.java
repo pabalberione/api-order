a17a73ce509aa6ab57d1cba59376a74335d1a344
Pabalberione <pabalberione@gmail.com>
2024-06-13 08:21:24 -0300

    First commit

---
+++ src/main/java/com/api_order/ApiOrderApplication.java
@@ -0,0 +1,13 @@
+package com.api_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_order/ApiOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
